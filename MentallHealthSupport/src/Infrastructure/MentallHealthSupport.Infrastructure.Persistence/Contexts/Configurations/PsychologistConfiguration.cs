b55c0afe60062a246873a63fe3a3a9ff96202635
inngutorova <inngutorova@gmail.com>
2024-03-14 22:38:04 +0300

    edited PsychologistConfiguration

--- MentallHealthSupport/src/Infrastructure/MentallHealthSupport.Infrastructure.Persistence/Contexts/Configurations/PsychologistConfiguration.cs
+++ MentallHealthSupport/src/Infrastructure/MentallHealthSupport.Infrastructure.Persistence/Contexts/Configurations/PsychologistConfiguration.cs
@@ -8,6 +8,15 @@ public class PsychologistConfiguration : IEntityTypeConfiguration<PsychologistMo
     public void Configure(EntityTypeBuilder<PsychologistModel> builder)
     {
         // прописать то, как все поля и constrains должны выглядеть в бд
+
+        builder.Property(p => p.Id).HasColumnName("psychologist_id");
+        builder.Property(p => p.Specialization).HasColumnName("specialization");
+        builder.Property(p => p.PricePerHour).HasColumnName("price_per_hour");
+        builder.Property(p => p.ExperienceStartDate).HasColumnName("experience_start_date");
+        builder.Property(p => p.ExperienceYears).HasColumnName("experience_years");
+        builder.Property(p => p.Rate).HasColumnName("rate");
+        builder.Property(p => p.User.Id).HasColumnName("user_id");
+        
         builder.HasKey(psychologist => psychologist.Id);
         builder.HasOne(psychologist => psychologist.User).WithOne(user => user.Psychologist);
         builder.HasMany(psychologist => psychologist.Reviews).WithOne(review => review.Psychologist);
